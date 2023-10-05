9ddbb4b8a278de8914cb1f169ebd2659b3de9262
807672412@qq.com <807672412@qq.com>
2023-10-05 11:04:53 +0800

    completed 404 page

--- v2-management/src/router/index.ts
+++ v2-management/src/router/index.ts
@@ -28,6 +28,10 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '*',
+    component: () => import(/* webpackChunkName: "NotFoundView" */ '../views/NotFoundView.vue')
   }
 ]
 
---
+++ v2-management/src/views/NotFoundView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <p>404</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFoundView',
+
+  data () {
+    return {
+
+    }
+  },
+
+  created () {
+    setTimeout(() => this.$router.push('/'), 3000)
+  },
+
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
